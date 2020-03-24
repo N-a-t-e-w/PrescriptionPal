b578e35e4b6348bb27e4662c2ddb39aa92d9bc27
Nicholas-C-Brown <nicholas.c.brown@alumni.ubc.ca>
2020-03-24 13:44:16 -0700

    upload all files
    weewoo

---
+++ app/src/main/java/cosc341/group4/prescriptionpal/HomepageActivity.java
@@ -0,0 +1,14 @@
+package cosc341.group4.prescriptionpal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomepageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+}
---
+++ app/src/main/java/cosc341/group4/prescriptionpal/TodayActivity.java
@@ -0,0 +1,14 @@
+package cosc341.group4.prescriptionpal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TodayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_today);
+    }
+}
